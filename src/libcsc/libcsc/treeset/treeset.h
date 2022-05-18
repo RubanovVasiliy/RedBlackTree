8f3627f7f0a15d4cb657ebd45d3d48e24a873abc
Rubanov Vasiliy <rubanovas2005@gmail.com>
2022-05-18 11:29:46 +0700

    init TreeSet

---
+++ src/libcsc/libcsc/treeset/treeset.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+
+namespace my_set {
+    template<typename T>
+    class TreeSet {
+
+    };
+}
---
+++ test/libcsc/libcsc/treeset/treeset.cpp
@@ -0,0 +1,7 @@
+#include <treeset/treeset.h>
+#include <gtest/gtest.h>
+
+
+TEST(first, first) {
+    ASSERT_EQ(1, 1);
+}
