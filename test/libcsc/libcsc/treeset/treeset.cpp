5be5ac0a33de2bc4090468a24bca9a45fa4978ac
Rubanov Vasiliy <rubanovas2005@gmail.com>
2022-05-19 00:53:04 +0700

    add rbtree structure

--- test/libcsc/libcsc/treeset/treeset.cpp
+++ test/libcsc/libcsc/treeset/treeset.cpp
@@ -1,7 +1,22 @@
 #include <treeset/treeset.h>
 #include <gtest/gtest.h>
+#include <vector>
 
 
 TEST(first, first) {
+    my_set::TreeSet set;
+    std::vector items = {7, 2, 6, 4, 5, 3, 1, 0};
+    for (auto &i: items) {
+        set.insert(i);
+    }
+    std::cout << set.size() << '\n';
+    set.remove(2);
+    set.print();
+    std::cout << "contains: " << set.contains(4) << '\n';
+    std::cout << set.size() << '\n';
+    set.clear();
+    std::cout << "contains: " << set.contains(4) << '\n';
+    std::cout << set.size() << '\n';
+
     ASSERT_EQ(1, 1);
 }
